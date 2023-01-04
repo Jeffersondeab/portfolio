ca38ed850f7abb48d955513d094bc1046545d057
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2023-01-04 20:28:52 -0300

    add

---
+++ script.js
@@ -0,0 +1,36 @@
+const experienceImages = document.querySelectorAll('.experienceImg')
+const popupWrapper = document.querySelector('.popup-wrapper')
+const headerContent = document.querySelector('.header-content')
+
+
+experienceImages.forEach((experienceImg) => {
+    experienceImg.addEventListener('click', () => {
+        popupWrapper.style.display = 'block'
+        experienceImg.style.transform = 'scale(2.5, 2.5)'
+        experienceImg.style.marginRight = '65%'
+        experienceImg.scrollIntoView()
+        experienceImg.style.marginTop = '5%'
+
+        if(window.innerWidth <= 500){
+            experienceImg.style.margin = 'auto'
+            experienceImg.style.display = 'flex'
+        }
+    })
+}) 
+
+
+
+
+popupWrapper.addEventListener('click', () =>{
+    popupWrapper.style.display = 'none'
+ 
+    experienceImages.forEach((experienceImg) =>{
+        experienceImg.style.transform = ''
+        experienceImg.style.marginTop = '0%'
+        experienceImg.style.margin = '0%'
+        experienceImg.style.display = 'inline'
+    })
+})
+
+
+ 
