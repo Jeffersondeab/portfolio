0b7a7f7b80074da1d4e6605f63c1151b9eb9658f
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2023-01-06 20:09:20 -0300

    add

--- JavaScript/modules/mainOpenBtn.js
+++ JavaScript/modules/mainOpenBtn.js
@@ -1,4 +1,6 @@
-const experience = document.querySelector('.experienceTitle')
+/* EXPERIENCIA */
+
+const experience = document.querySelector('.experience-btn')
 const experienceList = document.querySelector('.experience-list')
 
 experience.addEventListener('click', ()=>{
@@ -10,10 +12,75 @@ experience.addEventListener('click', ()=>{
 })
  
 
+ 
+
+/* SOBRE MIM */
+
+const aboutMeBtn = document.querySelector('.aboutMe-btn')
+const aboutMeContent = document.querySelector('.aboutMe-content')
+
+aboutMeBtn.addEventListener('click', ()=>{
+    if(aboutMeContent.style.display === 'block'){
+        aboutMeContent.style.display = 'none'
+    }else{
+        aboutMeContent.style.display = 'block'
+    }
+})
+
+
+/* LANGUAGENS QUE UTILIZO */
+
+
+const mytoolsBtn = document.querySelector('.mytools-btn')
+const mytoolsList = document.querySelector('.mytools-list')
+
+mytoolsBtn.addEventListener('click', ()=>{
+    if(mytoolsList.style.display === 'block'){
+        mytoolsList.style.display = 'none'
+    }else{
+        mytoolsList.style.display = 'block'
+    }
+})
+
+
+
+
+
+
+
+/* PDF */
+
+const mycurriculumBtn = document.querySelector('.MyCurriculum-btn')
+const mycurriculumContent = document.querySelector('.MyCurriculum-content')
+
+mycurriculumBtn.addEventListener('click', ()=>{
+    if(mycurriculumContent.style.display === 'block'){
+        mycurriculumContent.style.display = 'none'
+    }else{
+        mycurriculumContent.style.display = 'block'
+    }
+})
+
+
+
+
+
 function init(){
     experience
     experienceList
+
+    aboutMeBtn
+    aboutMeContent
+
+    mytoolsBtn
+    mytoolsList
+
+    mycurriculumBtn
+    mycurriculumContent
+    
 }
+
+
 
 init()
 
