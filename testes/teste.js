b12c310a0348ebf3c05fe8222d37880f63ccdf05
Jefferson Barcellos <jefferson_barcello@outlook.com>
2024-06-18 00:09:59 -0300

    add

--- testes/teste.js
+++ testes/teste.js
@@ -1,3 +1,25 @@
-var teste = document.querySelectorAll('teste')
+var teste = document.querySelectorAll('.teste')
+
+const sections = document.querySelectorAll('#teste01, #teste02, #teste03')
+
+
+function scroll(sec){
+    const bteste = document.getElementById(sec)
+    if(bteste){
+        bteste.scrollIntoView({ behavior: 'smooth'})
+    }
+}
+
+
+    teste.forEach(listteste =>{
+        listteste.addEventListener('click', () =>{
+            document.querySelector('.active')?.classList.remove('active')
+            listteste.classList.add('active')
+
+            const sec = listteste.getAttribute('data-teste')
+            scroll(sec)
+        })
+    })
+
 
 
