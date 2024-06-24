c70f2134298fd7531d00e4d6ef5442e5b92b1753
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2024-06-24 20:17:01 -0300

    add

--- assets/JavaScript/base.js
+++ assets/JavaScript/base.js
@@ -1,17 +1,12 @@
 
 import navegation from "./modules/navegation.js"
 /* import project from "../JavaScript/project.js/"
-import fontes from "../JavaScript/fontes.js/" */
+import fontes from "../JavaScript/fontes.js/"  */
 
 
 navegation.init()
-<<<<<<< HEAD
-project.init()
-fontes.init()
-
-
-
-=======
 /* project.init()
 fontes.init() */
->>>>>>> 86d5b56baba7daf7c9c9d9ebfa48af8c43f643b4
+
+
+ 
