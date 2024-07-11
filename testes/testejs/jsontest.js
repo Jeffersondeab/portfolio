827b74c8ba143fdc1dfb4e6e7b90e01350a5f801
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2024-07-11 20:06:09 -0300

    add

--- testes/testejs/jsontest.js
+++ testes/testejs/jsontest.js
@@ -1,4 +1,27 @@
 
+const teste = document.querySelector('.jsontest');
+
+fetch("teste.json")
+  .then((response) => response.json())
+  .then((dados) => {
+    dados.usuarios.map((usuario) => {
+      // Crie elementos HTML dinâmicos ou realize cálculos aqui
+      const nome = usuario.nome;
+      const paragrafo = document.createElement('p');
+      paragrafo.textContent = `Nome: ${nome}`;
+      teste.appendChild(paragrafo);
+    });
+  });
+
+
+
+
+
+
+
+
+
+/* 
 const teste = document.querySelector('.jsontest')
 
 fetch("teste.json").then((Response) => {
@@ -13,7 +36,7 @@ fetch("teste.json").then((Response) => {
 })
 
 
-
+ */
 
 function init (){
     teste
