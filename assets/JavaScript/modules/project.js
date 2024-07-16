322eff24fe52bfcfd3e8e69b1ae5307b991a212f
Jefferson Barcellos <jefferson_barcello@outlook.com>
2024-07-15 23:30:19 -0300

    add

--- assets/JavaScript/modules/project.js
+++ assets/JavaScript/modules/project.js
@@ -1,36 +1,42 @@
+
+
 function carregar(){
     fetch('get.json')
     .then(response => response.json())
     .then(jogos =>{
-        const container = document.querySelector('#myproject')
-
-        jogos.map(jogo => {
-            const card = document.createElement('div')
-            card.classList.add('card')
-
-            const img = document.createElement('img')
-            img.src = jogo.imagem
-            img.alt = jogo.nome
-
-            const titulo = document.createElement('h2')
-            titulo.textContent = jogo.nome
-
-            card.appendChild(img)
-            card.appendChild(titulo)
-            container.appendChild(card)
-        })
+       
+      const container = document.querySelector('#myproject')
+  
+      jogos.map(jogo => {
+        const card = document.createElement("div")
+  
+        card.classList.add("card")
+  
+        const img = document.createElement("img")
+  
+        img.src = jogo.imagem
+        img.alt = jogo.nome
+  
+        const titulo = document.createElement("h3")
+        titulo.textContent = jogo.nome;
+        
+        card.appendChild(img)
+        card.appendChild(titulo)
+  
+        container.appendChild(card)
+      })
     })
-    
-}
-
-
-function init(){
-    carregar()
-}
-
-init()
-
-export default{
-    init
-}
- 
+  }
+   
+  carregar()
+  
+  function init (){
+      carregar
+  }
+  
+  init()
+  
+  
+   export default{
+      init
+   }
