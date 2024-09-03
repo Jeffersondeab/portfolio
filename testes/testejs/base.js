b455f62890d2ebe9563843e93b11cc758581550b
Jefferson Barcellos <jefferson_barcello@outlook.com>
2024-09-02 23:18:56 -0300

    ADD

--- testes/testejs/base.js
+++ testes/testejs/base.js
@@ -1,5 +1,5 @@
 import teste from '../testejs/teste.js';
-import jsontest from './jsontest.js'
+/* import jsontest from './jsontest.js' */
 
 import tst from '../testejs/tst.js';
 
--- testes/testejs/tst.js
+++ testes/testejs/tst.js
@@ -1,21 +1,61 @@
 
 
 
+const frontButton = document.querySelector('front')
+const backButton = document.querySelector('back')
 
-frontButton.addEventListener('click', () => {
-  front.classList.add('active');
-  back.classList.remove('active');
-});
-
-backButton.addEventListener('click', () => {
-  front.classList.remove('active');
-  back.classList.add('active');
-});
 
 
 
 
 
+frontButton.addEventListener('click', () => {
+
+  
+    
+
+function carregar(){
+  fetch('teste.json')
+  .then(response => response.json())
+  .then(jogos =>{
+     
+    const container = document.querySelector('#frontBase')
+
+    jogos.map(jogo => {
+      const card = document.createElement("a")
+
+      card.classList.add("card")
+
+      const img = document.createElement("img")
+
+      img.src = jogo.imagem
+      img.alt = jogo.nome
+
+      const titulo = document.createElement("span")
+      titulo.textContent = jogo.nome;
+      
+      card.appendChild(img)
+      card.appendChild(titulo)
+
+      card.href = "" + jogo.url
+
+      container.appendChild(card)
+    })
+  })
+}
+
+
+  
+  if(.style.display === 'block'){
+    
+  }
+
+})
+
+
+
+ 
+carregar()
 
   
   function init (){
