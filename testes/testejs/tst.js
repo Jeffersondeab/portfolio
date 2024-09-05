f32923b4baf8bd2c7650a00fa40020368abb3aff
Jefferson Barcellos <jefferson_barcello@outlook.com>
2024-09-05 00:20:09 -0300

    ADD

--- testes/testejs/tst.js
+++ testes/testejs/tst.js
@@ -3,16 +3,55 @@ const teste = document.querySelector('.front')
 
 const txt = document.querySelector('.txt')
 
-teste.addEventListener('click', () => {
 
 
-  document.querySelector('.active')?.classList.remove('active');
-  txt.classList.add('active')
 
+
+
+function carregar(){
+  fetch('teste.json')
+  .then(response => response.json())
+  .then(jogos =>{
+
+
+
+     
+    const container = document.querySelector('#frontBase')
+
+    teste.addEventListener('click', () => {
+
+      container.classList.toggle('active');
   
+  })
+  
+    jogos.map(jogo => {
+      const card = document.createElement("a")
 
-})
+      card.classList.add("card")
 
+      const img = document.createElement("img")
+
+      img.src = jogo.imagem
+      img.alt = jogo.nome
+
+      const titulo = document.createElement("span")
+      titulo.textContent = jogo.nome;
+      
+      card.appendChild(img)
+      card.appendChild(titulo)
+
+      card.href = "" + jogo.url
+
+      container.appendChild(card)
+    })
+  })
+}
+
+carregar()
+ 
+
+
+ 
 
   
   function init (){
