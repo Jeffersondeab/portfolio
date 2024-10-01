11dba5c11678c61dbe0198e73433626bfc2237d1
Jefferson Barcellos <jefferson_barcello@outlook.com>
2024-10-01 00:11:12 -0300

    add

--- JavaScript/base.js
+++ JavaScript/base.js
@@ -1,6 +1,8 @@
 import ImagePopup from "./modules/ImagePopup.js"
 import mainOpenBtn from "./modules/mainOpenBtn.js"
+import mobile from "./modules/mobile.js"
 
+mobile.init()
 ImagePopup.init()
 mainOpenBtn.init() 
 
---
+++ JavaScript/modules/mobile.js
@@ -0,0 +1,16 @@
+const wrapper = document.querySelector('#wrapper')
+const nave = document.querySelector('#nav-list')
+const hamburgue = document.querySelector('#mobile-menu')
+
+
+hamburgue.addEventListener('click', () =>{
+    hamburgue.classList.toggle('active')
+    nave.classList.toggle('active')
+    wrapper.classList.toggle('active')
+})
+
+wrapper.addEventListener('click', () =>{
+    wrapper.classList.toggle('active')
+    hamburgue.classList.toggle('active')
+    nave.classList.toggle('active')
+})
